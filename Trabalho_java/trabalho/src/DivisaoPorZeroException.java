2f9e0e8ec2a824fa08b595e9fdb9e5b344452ef9
Reis-exp <nicolasmoreira2015@hotmail.com>
2026-03-22 11:19:10 -0300

    Delete Trabalho_java/trabalho directory

--- Trabalho_java/trabalho/src/DivisaoPorZeroException.java
+++
@@ -1,5 +0,0 @@
-public class DivisaoPorZeroException extends Exception {
-    public DivisaoPorZeroException(String message){
-        super(message);
-    }
-}
--- Trabalho_java/trabalho/src/OperacaoMatematica.java
+++
@@ -1,8 +0,0 @@
-abstract class OperacaoMatematica {
-    public abstract double calcular(double a, double b) throws DivisaoPorZeroException;
-
-    public void exibirResultado(double resultado){
-        if (resultado != 0)
-        System.out.println("O resultado da conta é:" + resultado);
-    }
-}
